0b4d3348b19bd91e73861b1c1077277d31edd959
znpsc <noemail@email.com>
2018-03-07 18:23:53 +0100

    dynamic test update 2

--- src/test/java/com/example/calculator/CalculatorDynamicTest.java
+++ src/test/java/com/example/calculator/CalculatorDynamicTest.java
@@ -4,17 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 @DisplayName("Basic calculus for showing example of dynamic test")
@@ -41,19 +38,35 @@ public class CalculatorDynamicTest {
 
     @TestFactory
     @DisplayName("for mutliplication return stream")
-    Stream<DynamicTest> dynamicTestStream(){
+    Stream<DynamicTest> dynamicTestStream() {
 
 
+        int init = 10;
+        List<DynamicTest> dynamicTestList = new ArrayList<>();
+        for (int i = 0; i < init; i++) {
+            dynamicTestList.add(generateXMultiplies(i, init, i * init));
+        }
 
 
+        return Stream.of(dynamicTest("aaa", () -> generateXMultiplies(1, 1, 1)));
+    }
+
+    @TestFactory
+    @DisplayName("")
+    Iterable<DynamicTest> dynamicTestIterable() {
+        return null;
+    }
+
+    @TestFactory
+    @DisplayName("")
+    Iterator<DynamicTest> dynamicTestIterator() {
+        return null;
     }
 
     //TODO name of them method should be refactor
     private DynamicTest generateXMultiplies(int input, int multiplicator, int result) {
         return dynamicTest("Case#" + input + ": " + input + "*" + multiplicator + "=" + result, () -> assertEquals(result, input * multiplicator));
     }
-
-
 
 
 }
