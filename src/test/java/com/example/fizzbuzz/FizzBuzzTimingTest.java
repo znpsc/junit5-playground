1dfa46c65b9d40bedc9463d00a0d0b353c70c511
znpsc <noemail@email.com>
2018-03-11 22:02:51 +0100

    cleaning tests before presentation

--- src/test/java/com/example/fizzbuzz/FizzBuzzTimingTest.java
+++ src/test/java/com/example/fizzbuzz/FizzBuzzTimingTest.java
@@ -1,32 +1,8 @@
 package com.example.fizzbuzz;
 
 import com.example.fizzbuzz.extensions.FizzBuzzTimeMeasurementExtension;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(FizzBuzzTimeMeasurementExtension.class)
 public class FizzBuzzTimingTest {
-
-    FizzBuzzProblem sut;
-
-    @BeforeEach
-    void setUp() throws InterruptedException {
-        sut = new FizzBuzzProblem();
-        Thread.sleep(1000); //cheating
-    }
-
-    @Test
-    void testFizzNumbers() throws InterruptedException {
-        assertEquals("Fizz", sut.getFizzBuzzNumber(3));
-        Thread.sleep(500); //cheating
-    }
-
-    @Test
-    void testBuzzNumbers() throws InterruptedException {
-        assertEquals("Buzz", sut.getFizzBuzzNumber(25));
-        Thread.sleep(500); //cheating
-    }
 }
